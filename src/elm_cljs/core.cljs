72734db96eb1544f9691f817a669357aea4971f6
Amitay Horwitz <amitayh@gmail.com>
2017-08-16 15:12:07 +0300

    added effect handling

--- src/elm_cljs/core.cljs
+++ src/elm_cljs/core.cljs
@@ -10,6 +10,34 @@
 
 (def messages (chan))
 
+(def effects (chan))
+
+(defprotocol Effect
+  (run-effect [this]))
+
+(defrecord Random [callback]
+  Effect
+  (run-effect [this]
+    ((:callback this) (rand-int 100))))
+
+(defrecord Print []
+  Effect
+  (run-effect [this]
+    (println "Printed!")))
+
+(defn printaction [action old-model new-model effect]
+  (js/console.groupCollapsed action)
+  (js/console.info old-model)
+  (js/console.info new-model)
+  (js/console.info effect)
+  (js/console.groupEnd))
+
+(go
+  (while true
+    (let [effect (<! effects)
+          message (run-effect effect)]
+      (if-not (nil? message) (>! messages message)))))
+
 (def root (.getElementById js/document "app"))
 
 (defn submit [message]
@@ -17,9 +45,12 @@
 
 (defn main [initial-model view update]
   (go-loop [model initial-model]
-           (println model)
            (q/render (view model) root)
-           (recur (update model (<! messages)))))
+           (let [message (<! messages)
+                 [updated-model effect] (update model message)]
+             (printaction message model updated-model effect)
+             (if-not (nil? effect) (>! effects effect))
+             (recur updated-model))))
 
 ;; ******************************************************************
 
@@ -31,21 +62,32 @@
 (defrecord Increment []
   Action
   (run [this model]
-    (update model :counter inc)))
+    [(update model :counter inc) nil]))
 
 (defrecord Decrement []
   Action
   (run [this model]
-    (update model :counter dec)))
+    [(update model :counter dec) nil]))
+
+(defrecord SetCounter [value]
+  Action
+  (run [this model]
+    [(assoc model :counter value) nil]))
 
 (defrecord GenerateRandom []
   Action
-  (run [this model] model))
+  (run [this model]
+    [model (->Random ->SetCounter)]))
+
+(defrecord PrintSomething []
+  Action
+  (run [this model]
+    [model (->Print)]))
 
 (defrecord Type [text]
   Action
   (run [this model]
-    (assoc model :text (:text this))))
+    [(assoc model :text (:text this)) nil]))
 
 (defn view [model]
   (d/div {}
@@ -54,6 +96,7 @@
               (:counter model)
               (d/button {:onClick (fn [e] (submit (->Increment)))} "+"))
          (d/p {} (d/button {:onClick (fn [e] (submit (->GenerateRandom)))} "Random"))
+         (d/p {} (d/button {:onClick (fn [e] (submit (->PrintSomething)))} "Print something"))
          (d/p {}
               (d/input {:onChange (fn [e] (let [value (-> e (.-target) (.-value))]
                                             (submit (->Type value))))
